963e294c7c929b420d04879d84fd2436c707f16a
nezhinsky <m.nezhinsky@yandex.ru>
2023-01-25 01:19:12 +0400

    add user administration add recipes administration

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from recipes import models
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    """
+    Администрирование рецептов.
+    """
+    list_display = ('name', 'id', 'author', 'text', 'image')
+    list_filter = ('author', 'name', 'tags')
+
+
+@admin.register(models.Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    """
+    Администрирование ингредиентов.
+    """
+    list_display = ('name', 'measurement_unit')
+    list_filter = ('name', 'measurement_unit')
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    Администрирование тегов.
+    """
+    list_display = ('name', 'color', 'slug')
+
+
+@admin.register(models.Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    """
+    Администрирование добавления рецептов в избранное.
+    """
+    list_display = ('user', 'recipe')
+
+
+@admin.register(models.IngredientInRecipe)
+class IngredientInRecipe(admin.ModelAdmin):
+    """
+    Администрирование ингредиентов в рецептах.
+    """
+    list_display = ('recipe', 'ingredient', 'amount')
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import Follow
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    """
+    Администрирование пользователей.
+    """
+    list_display = ('username', 'id', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email', 'first_name')
+
+
+@admin.register(Follow)
+class SubscribeAdmin(admin.ModelAdmin):
+    """
+    Администрирование подписок на авторов.
+    """
+    list_display = ('user', 'author')
