88443de974c1d8bf34fe05ff3010a1129889b772
nezhinsky <m.nezhinsky@yandex.ru>
2023-01-29 22:06:04 +0400

    refactor for redoc

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ foodgram-project-react - 17ый спринт
 Дипломный проект
 
 ![example workflow](https://github.com/Nezhinskiy/foodgram-project-react/actions/workflows/foodgram-project-react_workflow.yml/badge.svg)
+
+[Перейти на сайт](http://158.160.26.251/recipes)
+
+[Документация к API](http://158.160.26.251/api/docs/redoc)
--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -2,7 +2,7 @@ import json
 
 from django.core.management.base import BaseCommand
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
 
 
 class Command(BaseCommand):
@@ -14,4 +14,10 @@ class Command(BaseCommand):
             ingredients_data = json.loads(ingredients_json_data.read())
             for ingredients in ingredients_data:
                 Ingredient.objects.get_or_create(**ingredients)
+
+        with open('data/tags.json', encoding='utf-8') as dtags_json_data:
+            tags_data = json.loads(dtags_json_data.read())
+            for tags in tags_data:
+                Tag.objects.get_or_create(**tags)
+
         self.stdout.write(self.style.SUCCESS('Данные загружены'))
